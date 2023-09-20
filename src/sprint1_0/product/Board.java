dfb19be1c7acff114b356df0e5a8815951e69dfd
Regina Ho <uhhvng@umkc.edu>
2023-09-20 15:38:54 -0500

    commit

---
+++ src/sprint1_0/product/Board.java
@@ -0,0 +1,15 @@
+package sprint1_0.product;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Board extends JPanel {
+    public Board(){
+        setBackground(Color.PINK);
+        setPreferredSize(new Dimension(200,200));
+        Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+        setBorder(blackline);
+
+    }
+}
---
+++ src/sprint1_0/product/LeftPlayer.java
@@ -0,0 +1,49 @@
+package sprint1_0.product;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LeftPlayer extends JPanel {
+    GridLayout left_panel = new GridLayout(0,2);
+    JLabel playerLabel;
+    JRadioButton r_button1;
+    JRadioButton r_button2;
+    JCheckBox checkBox;
+    JRadioButton r_button3;
+    JRadioButton r_button4;
+    public LeftPlayer(){
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        playerLabel= new JLabel("Red Player");
+        playerLabel.setForeground(Color.WHITE);
+        playerLabel.setBackground(Color.RED);
+        playerLabel.setOpaque(true);
+
+        r_button1 = new JRadioButton("S");
+        r_button2 = new JRadioButton("O");
+        r_button3 = new JRadioButton("Human");
+        r_button4 = new JRadioButton("Computer");
+
+        checkBox = new JCheckBox("Check Box");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(r_button1);
+        group.add(r_button2);
+
+        ButtonGroup group2 = new ButtonGroup();
+        group2.add(r_button3);
+        group2.add(r_button4);
+
+        add(playerLabel);
+        add(r_button1);
+        add(r_button2);
+        add(r_button3);
+        add(r_button4);
+
+        add(checkBox);
+
+        r_button1.doClick(); //set default Radio button
+
+    }
+
+}
